d3bbcd2bfcf64bf1a405a0437841bfb2630eb814
Emily Bossiere <emilybossiere@gmail.com>
2019-04-26 11:11:24 -0700

    search functionality through API created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+//import axios from "axios";
+//https://www.ifixit.com/api/2.0/categories/all
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    search: "",
+    devices: []
+  };
+
+  onChange = e => {
+    const { value } = e.target;
+    this.setState({
+      search: value
+    });
+
+    this.search(value);
+  };
+
+  search = search => {
+    const url = `https://www.ifixit.com/api/2.0/suggest/${search}?doctypes=device`;
+
+    fetch(url)
+      .then(results => results.json())
+      .then(data => {
+        this.setState({ devices: data.results });
+      });
+  };
+
+  componentDidMount() {
+    this.search("");
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search for devices..."
+          onChange={this.onChange}
+        />
+        {this.state.devices.map(device => (
+          <ul key={device.title}>
+            <li>{device.title}</li>
+          </ul>
+        ))}
+      </form>
+    );
+  }
 }
 
 export default App;

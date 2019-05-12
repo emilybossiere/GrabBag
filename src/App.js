847f5310c7aeeb65cf89e1a058295579ba43a697
Emily Bossiere <emilybossiere@gmail.com>
2019-05-12 10:49:20 -0700

    final commit (hopefully)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     tempKeys: []
   };
 
+  //take in search value
   onChange = e => {
     const { value } = e.target;
     this.setState({
@@ -19,6 +20,7 @@ class App extends React.Component {
     this.search(value);
   };
 
+  //search for typed query
   search = search => {
     const url = `https://www.ifixit.com/api/2.0/suggest/${search}?doctypes=device`;
     fetch(url)
@@ -28,8 +30,10 @@ class App extends React.Component {
       });
   };
 
+  //add selected device to bag
   addDevice = (e, deviceTitle) => {
     const array = Array.from(this.state.bag || []);
+    //only add if item is not already in bag
     if (array.indexOf(deviceTitle) === -1) {
       array.push(deviceTitle);
     } else {
@@ -41,7 +45,7 @@ class App extends React.Component {
     });
   };
 
-
+  //remove selected device from bag
   removeDevice = (e, deviceTitle) => {
     this.setState(prevState => ({
       bag: prevState.bag.filter(d => d !== deviceTitle)
@@ -52,6 +56,7 @@ class App extends React.Component {
     localStorage.setItem("list", JSON.stringify(removedDeviceArray));
   };
 
+  //clear contents of bag
   removeAll = e => {
     this.setState({
       bag: []
@@ -62,12 +67,15 @@ class App extends React.Component {
     localStorage.setItem("list", JSON.stringify(clearedArray));
   };
 
+  //create the next layer of nested menu recursively
   makeMenuLayer = layer => {
     const { objectKeys } = this.state;
     if (layer == null) {
+      //used to fix null object error
       return null;
     }
     const layerKeys = Object.entries(layer).map(([key, value]) => {
+      //if object has children, display arrow icon, else do nothing
       var arrow = Object.keys(value).length ? (
         <i
           className="fas fa-angle-right"
@@ -76,19 +84,23 @@ class App extends React.Component {
       ) : (
         ""
       );
+      //if object doesn't have children, display + icon, else do nothing
       var plus =
         Object.keys(value).length === 0 ? (
           <i
-            className="fas fa-plus"
-            style={{ cursor: "pointer", color: "green" }}
+            className="fas fa-plus plus"
+            style={{ cursor: "pointer" }}
             onClick={e => this.addDevice(e, key)}
           />
         ) : (
           ""
         );
       return (
-        <ul key={key}>
-          <div onClick={() => this.handleShowMore(key)}>
+        <ul className="menu-items" key={key}>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => this.handleShowMore(key)}
+          >
             {key} {arrow} {plus}
           </div>
 
@@ -137,6 +149,7 @@ class App extends React.Component {
     const bag = storedList;
     this.setState({ bag });
 
+    //api call to categories for menu
     axios.get("https://www.ifixit.com/api/2.0/categories").then(response => {
       this.setState({ categories: response.data });
     });
@@ -145,9 +158,11 @@ class App extends React.Component {
     this.initializeObjectKeys();
     this.setState({ categories });
   }
+
   render() {
     return (
       <div className="text">
+        {/*header containing logo & title */}
         <header className="header">
           <img
             className="header-img"
@@ -155,11 +170,15 @@ class App extends React.Component {
             src={"ifixit-logo.png"}
             alt="Logo"
           />
-          GRAB BAG
+          <h>GRAB BAG</h>
         </header>
+
+        {/* div for search bar, menu, and bag */}
         <div className="flex-container">
           <h3>Search for a device...</h3>
-          <h3>
+
+          {/* bag and remove all button */}
+          <h3 className="your-bag">
             <span>Your Bag</span>
             <button className="btn button" onClick={e => this.removeAll(e)}>
               Remove all
@@ -172,10 +191,11 @@ class App extends React.Component {
             <input
               className="rounded search"
               type="text"
-              placeholder="Search..."
+              placeholder="Search"
               onChange={this.onChange}
             />
-            <div className="search-results">
+            {/* add devices from search bar */}
+            <div>
               {(this.state.devices || []).map(device => (
                 // eslint-disable-next-line
                 <a key={device.title}>
@@ -183,25 +203,28 @@ class App extends React.Component {
                     {device.title}{" "}
                     <i
                       className="fas fa-plus plus input"
-                      style={{ cursor: "pointer", color: "green" }}
+                      style={{ cursor: "pointer" }}
                       onClick={e => this.addDevice(e, device.title)}
                     />
                   </li>
                 </a>
               ))}
+              {/* create menu using categories */}
+              <h4>Or browse for a device...</h4>
+              <div className="menu">
+                {this.makeMenuLayer(this.state.categories)}
+              </div>
             </div>
-            <h3>Or browse for a device below...</h3>
-            {this.makeMenuLayer(this.state.categories)}
           </form>
-
-          <div>
+          {/* populated/unpopulated bag and single device removal */}
+          <div className="bag">
             <div>
               {(this.state.bag || []).map(device => (
                 <p key={device}>
                   {device}
                   <i
                     className="fas fa-times ex"
-                    style={{ cursor: "pointer", color: "red" }}
+                    style={{ cursor: "pointer" }}
                     onClick={e => this.removeDevice(e, device)}
                   />
                 </p>
@@ -209,8 +232,6 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-
-        {/*<Menu />*/}
       </div>
     );
   }

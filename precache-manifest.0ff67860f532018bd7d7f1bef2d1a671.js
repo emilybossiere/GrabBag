c6e5f499c409ff20479cf6908297587e4ff62354
Emily Bossiere <emilybossiere@gmail.com>
2019-05-13 15:45:56 -0700

    Updates

---
+++ precache-manifest.0ff67860f532018bd7d7f1bef2d1a671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715e42c4634b17a46bad2ffa57bc01b5",
+    "url": "/GrabBag/index.html"
+  },
+  {
+    "revision": "323ba0a132a04b491a21",
+    "url": "/GrabBag/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f006e618b77354254610",
+    "url": "/GrabBag/static/css/main.82e74435.chunk.css"
+  },
+  {
+    "revision": "323ba0a132a04b491a21",
+    "url": "/GrabBag/static/js/2.578f8cdc.chunk.js"
+  },
+  {
+    "revision": "f006e618b77354254610",
+    "url": "/GrabBag/static/js/main.65d2b559.chunk.js"
+  },
+  {
+    "revision": "e5ceb82587ac3ec6b161",
+    "url": "/GrabBag/static/js/runtime~main.f4fac140.js"
+  }
+]);
